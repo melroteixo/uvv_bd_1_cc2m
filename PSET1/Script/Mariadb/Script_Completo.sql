ca1f6410e33809f836e1cb1f87fec95a9471b72b
melroteixo <viniciust.melo@icloud.com>
2022-04-25 17:03:45 -0300

    Correcao do script completp

--- PSET1/Script/Mariadb/Script_Completo.sql
+++ PSET1/Script/Mariadb/Script_Completo.sql
@@ -1,10 +1,14 @@
-CREATE USER vtm IDENTIFIED BY '';
+-- Cria usuário do SGBD
+CREATE USER vinicius IDENTIFIED BY '';
 
-GRANT ALL PRIVILEGES ON *.* TO vtm;
---Entrar com senha: vtm
-SYSTEM mysql -u vtm -p;
-
+-- Cria Banco de dados UVV com suas propriedades e permissões
 CREATE DATABASE uvv CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Concede todos privilégios de administrador ao usuário
+GRANT ALL PRIVILEGES ON uvv.* TO vinicius; 
+
+-- Entra com o usuário criado no SGBD
+SYSTEM mysql -u vinicius -p;
 
 
 
