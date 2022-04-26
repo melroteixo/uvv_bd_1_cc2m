3904a3cc321f1de596c66a1bfbc0a9b33336eb5b
melroteixo <viniciust.melo@icloud.com>
2022-04-26 20:58:53 -0300

    alteracao create database e grant all - mariabd - completo

--- PSET1/Script/Mariadb/Script_Completo.sql
+++ PSET1/Script/Mariadb/Script_Completo.sql
@@ -1,11 +1,11 @@
 -- Cria usuário do SGBD
 CREATE USER vinicius IDENTIFIED BY '';
 
--- Concede todos privilégios de administrador ao usuário
-GRANT ALL PRIVILEGES ON uvv.* TO vinicius; 
-
 -- Cria Banco de dados UVV com suas propriedades e permissões
 CREATE DATABASE uvv CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Concede todos privilégios de administrador ao usuário
+GRANT ALL PRIVILEGES ON uvv.* TO vinicius; 
 
 
 --Conectando ao banco de dados uvv
